da1b29058ade5687ee86860014f33e3939283461
Katerina Karagiannopoulou <32834432+Karagiannopoulou@users.noreply.github.com>
2020-02-24 10:43:00 +0200

    Update zonalStastics.py

--- zonalStastics.py
+++ zonalStastics.py
@@ -19,189 +19,136 @@ arcpy.env.overwriteOutput = True
 # Start time
 start_time = time.time()
 
-# Set up the variables
-firePoints = r'D:\FireHazard\firepoints\centr_msg_date_wgs.shp'
-fc_schema = r'C:\Users\noa\eclipse-workspace\ZonalStatistics\output\fc_schema.shp'
+# Set up global variables
 maindir = r'Y:\data\KaterinaKarag\2018'
-output_folder = r'C:\Users\noa\eclipse-workspace\ZonalStatistics\output'
+firePoints = r'D:\FireHazard\firepoints\centr_msg_date_wgs.shp'
+fc_schema = r'C:\Users\noa\eclipse-workspace\ZonalStatistics\output\fc_schema.shp' # standardized shapefile's schema (attribute table),which will denote empty's shapefile schema. 
+output_folder = r'C:\Users\noa\eclipse-workspace\ZonalStatistics\output' # setup where you want to save the output shapefile with the ndvi values. 
 
 
-# pre-built functions
-dates_from_vector = fireDates(firePoints, "firedate_g")
-rasterInfo = raster_info(maindir, '.hdf')
-dates_dictionary = compare2lists(dates_from_vector, rasterInfo)
+# call build-in functions
 
 
-# create a temporary shape file
-try:
-    
-    outputname = 'fc_ndvi.shp'
-    spatial_ref = arcpy.Describe(firePoints).spatialReference
-    emptyFC = arcpy.CreateFeatureclass_management(output_folder, outputname, "POINT", fc_schema, "DISABLED", "DISABLED", spatial_ref)
-     
-     
-except:
-    print arcpy.GetMessages() 
-     
-    print ('error has occurred')
-
-filenamesList = []    
-
-for keys, values in dates_dictionary.items():
-    
-
-    img01 = str(values[1][0].split(".hdf")[0]) + "_prj.tif"
-    img02 = str(values[1][1].split(".hdf")[0]) + "_prj.tif"
-    img03 = str(values[1][2].split(".hdf")[0]) + "_prj.tif"
-    
-    
-    name1 = img01[17:23]
-    name2 = img02[17:23]
-    name3 = img03[17:23]                
-                    
-    print(name1)
-    print(name2)
-    print(name3)
-                    
-    
-
+# create an empty shape file to be filled with the ndvi values
+def emptyFC(firePoints, default_schema):
     try:
-        arcpy.MakeFeatureLayer_management(firePoints,"firePoints_lyr")
-        query = """ "firedate_g" = '%s'"""%keys
-        
-        print(query)
-        
-        arcpy.SelectLayerByAttribute_management("firePoints_lyr", "NEW_SELECTION", query)
-        
-        
-        for folderPath, folderNames, fileNames in os.walk(maindir):
-            
-            for prj_images in fileNames:
-                
-                
-                if prj_images == img01:
-                    
-                    workspace = env.workspace = folderPath
-                    raster_images = arcpy.Raster(prj_images)                 
-                    
-                    outputName1 = "fc_" + keys + name1 + '.shp'
-                    
- 
-                    extracted_vector1 = os.path.join(output_folder, outputName1) 
-                    arcpy.gp.ExtractValuesToPoints_sa("firePoints_lyr", raster_images, extracted_vector1, "NONE", "VALUE_ONLY")
-                    
-                    print("extracted vector1 {}".format(extracted_vector1))
-
-                    arcpy.Append_management(extracted_vector1 ,emptyFC,'TEST')
-                   
-
-                    
-                    
-                    
-                if prj_images == img02:
-                    
-                    workspace = env.workspace = folderPath
-                    raster_images = arcpy.Raster(prj_images)
-                    
-                    
-                    outputName2 = "fc_" + keys + name2 + '.shp'
-                    
-                    extracted_vector2 = os.path.join(output_folder, outputName2)
-                    
-                    arcpy.gp.ExtractValuesToPoints_sa("firePoints_lyr", raster_images, extracted_vector2, "NONE", "VALUE_ONLY")
-                    
-                    print("extracted vector2 {}".format(extracted_vector2))
-
-                    arcpy.Append_management(extracted_vector2, emptyFC,'TEST')
-                    
-                    
-                    
-                    
-                if  prj_images == img03:
-                    
-                    workspace = env.workspace = folderPath
-                    raster_images = arcpy.Raster(prj_images)
-                    
-                    outputName3 = "fc_" + keys + name3 + '.shp'
-                    extracted_vector3 = os.path.join(output_folder, outputName3)
-                    
-                    arcpy.gp.ExtractValuesToPoints_sa("firePoints_lyr", raster_images, extracted_vector3, "NONE", "VALUE_ONLY")
-                    
-                    print("extracted vector3 {}".format(extracted_vector3))
-                    
-                    arcpy.Append_management(extracted_vector3 ,emptyFC,'TEST')
-                       
-                    
-                    
-                    
-                    
-                    
-                    
-
-        
+        outputname = 'fc_ndvi.shp'
+        spatial_ref = arcpy.Describe(firePoints).spatialReference
+        emptyFC = arcpy.CreateFeatureclass_management(output_folder, outputname, "POINT", default_schema, "DISABLED", "DISABLED", spatial_ref)
+         
+         
     except:
-        print arcpy.GetMessages()
-        
-        
-        
-            
+        print arcpy.GetMessages() 
     
-    
-#     arcpy.gp.ExtractValuesToPoints_sa()
-    
-# for keys, values in dates_dictionary.items():
-    
-    
-#     
-    
-#     print(type(values[0]))
+    return emptyFC
 
 
-            
-            
-       
-            
-            
-        
-        
+def zonalndvi(emptyFC, dictionary):  
+
+    for keys, values in dictionary.items():
         
     
-#     
-# 
-#         arrays_withnames = [arrays for arrays in values[1]]
-#         
-#         print arrays_withnames
-#     
+        img01 = str(values[1][0].split(".hdf")[0]) + "_prj.tif"
+        img02 = str(values[1][1].split(".hdf")[0]) + "_prj.tif"
+        img03 = str(values[1][2].split(".hdf")[0]) + "_prj.tif"
+        
+        
+        name1 = img01[17:23]
+        name2 = img02[17:23]
+        name3 = img03[17:23]                  
+    
+        try:
+            arcpy.MakeFeatureLayer_management(firePoints,"firePoints_lyr")
+            query = """ "firedate_g" = '%s'"""%keys
+            
+            print(query)
+            
+            arcpy.SelectLayerByAttribute_management("firePoints_lyr", "NEW_SELECTION", query)
+            
+            
+            for folderPath, folderNames, fileNames in os.walk(maindir):
+                
+                for prj_images in fileNames:
+                    
+                    
+                    if prj_images == img01:
+                        
+                        workspace = env.workspace = folderPath
+                        raster_images = arcpy.Raster(prj_images)                 
+                        
+                        outputName1 = "fc_" + keys + name1 + '.shp'
+                        
      
+                        extracted_vector1 = os.path.join(output_folder, outputName1) 
+                        arcpy.gp.ExtractValuesToPoints_sa("firePoints_lyr", raster_images, extracted_vector1, "NONE", "VALUE_ONLY")
+                        
+                        print("extracted vector1 {}".format(extracted_vector1))
+    
+                        arcpy.Append_management(extracted_vector1 ,emptyFC,'TEST')
+                        
+                        
+                        arcpy.Delete_management(extracted_vector1)
+    
+                        
+                    if prj_images == img02:
+                        
+                        workspace = env.workspace = folderPath
+                        raster_images = arcpy.Raster(prj_images)
+                        
+                        
+                        outputName2 = "fc_" + keys + name2 + '.shp'
+                        
+                        extracted_vector2 = os.path.join(output_folder, outputName2)
+                        
+                        arcpy.gp.ExtractValuesToPoints_sa("firePoints_lyr", raster_images, extracted_vector2, "NONE", "VALUE_ONLY")
+                        
+                        print("extracted vector2 {}".format(extracted_vector2))
+    
+                        arcpy.Append_management(extracted_vector2, emptyFC,'TEST')
+                        
+                        arcpy.Delete_management(extracted_vector2)
+                        
+                        
+                    if  prj_images == img03:
+                        
+                        workspace = env.workspace = folderPath
+                        raster_images = arcpy.Raster(prj_images)
+                        
+                        outputName3 = "fc_" + keys + name3 + '.shp'
+                        extracted_vector3 = os.path.join(output_folder, outputName3)
+                        
+                        arcpy.gp.ExtractValuesToPoints_sa("firePoints_lyr", raster_images, extracted_vector3, "NONE", "VALUE_ONLY")
+                        
+                        print("extracted vector3 {}".format(extracted_vector3))
+                        
+                        arcpy.Append_management(extracted_vector3 ,emptyFC,'TEST')
+                        
+                        arcpy.Delete_management(extracted_vector3)
 
+            
+        except:
+            print arcpy.GetMessages()
+            
+            
+            
+                
+    return  emptyFC   
+        
 
+ 
+def main():
+    dates_from_vector = fireDates(firePoints, "firedate_g")
+    rasterInfo = raster_info(maindir, '.hdf')
+    dates_dictionary = compare2lists(dates_from_vector, rasterInfo)
+    emptyFC = emptyFC(firePoints, fc_schema)
+    FC_withNDVI = zonalndvi(emptyFC, dates_dictionary)
+    
 
-
-#     
-#     
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# def main():
-#     
-# 
-# 
-# if __name__ == "__main__":
-#     main()
+ 
+if __name__ == "__main__":
+    main()
 
 arcpy.CheckInExtension('Spatial')
 
 elapsed_time = time.time() - start_time
-print
-time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+print (time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
