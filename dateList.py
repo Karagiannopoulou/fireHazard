d46399fdc60b3f0c5ede5068af10e475d1bb1316
Katerina Karagiannopoulou <32834432+Karagiannopoulou@users.noreply.github.com>
2020-02-28 17:10:46 +0200

    Update dateList.py

--- dateList.py
+++ dateList.py
@@ -72,3 +72,56 @@ def raster_info(input2, wildcard):
 
 
 def compare2lists(vectorDates, rasterInfo):
+    # To vector array einai ta dates me tis fwties
+    vector_array = np.sort(np.array(vectorDates))
+
+    # Ta raster dates tha einai mia katheth lista me tis hmeromhnies to raster, kai ta filenames ta onomata
+    raster_dates = [];
+    fileNames = []
+
+    for count, i in enumerate(rasterInfo):
+        if count % 2 == 1:
+            raster_dates.append(i)
+
+        # edw me to mod, lew opou h thesh einai odd einai raster date kai opou zugh einai filename
+
+        elif count % 2 == 0:
+            fileNames.append(i)
+
+    raster_dates_array = np.vstack(np.array(raster_dates))
+    fileNames_array = np.vstack(np.array(fileNames))
+    rasters_array = np.column_stack((raster_dates_array, fileNames_array))
+
+    min_date = []
+    dates_dictionary = {}
+
+    for firedate in vector_array:
+        for ndvi_dates in raster_dates_array[:, 0]:
+
+            if ndvi_dates < firedate:
+                min_date.append(ndvi_dates)
+                prev_NDVI = max(min_date)
+
+                tmp = {}
+                tmp[firedate] = prev_NDVI
+
+                dates_dictionary.update(tmp)
+
+    for keys, values in dates_dictionary.items():
+        # print ("days {}".format(values))
+
+        max_days_index = np.where(rasters_array[:, 0] == values)
+        max_fileNames = rasters_array[:, 1][max_days_index]
+
+        tmp2 = {}
+        tmp2[keys] = values, max_fileNames
+
+        dates_dictionary.update(tmp2)
+
+    #     print dates_dictionary
+    return dates_dictionary
+
+
+
+
+       
